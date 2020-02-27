54c2439dd856e838908e1e5c8a44fa6fcaef6db4
Andy Gallagher <andy.gallagher@theguardian.com>
2020-02-27 15:52:29 +0000

    upload to existing path if it's present in the record

--- docker/awsupload/do_upload.sh
+++ docker/awsupload/do_upload.sh
@@ -5,6 +5,15 @@ echo ${TRANSCODED_MEDIA}
 ls -lh "${TRANSCODED_MEDIA}"
 echo ${THUMBNAIL_IMAGE}
 ls -lh "${THUMBNAIL_IMAGE}"
+
+if [ "${MEDIA_FILE}" != "" ]; then
+  #make sure any leading / is removed as s3 does not really like these (well s3 does not technically care but it makes finding stuff a pain)
+    OUTPUT_PATH=`dirname "${MEDIA_FILE}" | sed  's/^\///'`
+    echo Using output path ${OUTPUT_PATH} from media file path
+else
+    echo Using output path ${OUTPUT_PATH} from settings
+fi
+
 echo Uploading \"${TRANSCODED_MEDIA}\" to \"${OUTPUT_PATH}\" on \"${OUTPUT_BUCKET}\"...
 aws s3 cp "${TRANSCODED_MEDIA}" "s3://${OUTPUT_BUCKET}/${OUTPUT_PATH}/`basename ${MEDIA_FILE}`"
 
