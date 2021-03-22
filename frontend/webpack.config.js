809a141de55ec96126b0e4f85350087e0f518318
Andy Gallagher <andy.gallagher@theguardian.com>
2021-03-22 11:25:13 +0000

    updates to webpack, babel

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -12,6 +12,15 @@ var config = {
         path: BUILD_DIR,
         filename: 'bundle.js'
     },
+    resolve: {
+        extensions: [".js",".jsx",".svg"],
+        fallback: {
+            util: require.resolve("util/"),
+            crypto: require.resolve("crypto-browserify"),
+            buffer: require.resolve("buffer/")
+        },
+    },
+
     optimization: {
         minimizer: [new TerserPlugin()]
     },
@@ -31,7 +40,9 @@ var config = {
         ]
     },
     plugins: [
-        new ExtractTextPlugin(BUILD_DIR + "styles.css")
+        new webpack.ProvidePlugin({
+            process: "process/browser",
+        }),
     ]
 };
 
